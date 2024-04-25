9daf74bb78fc6d9c0c9bcf20b97ebacc593291b7
fifizinho <132359775+fifizinho@users.noreply.github.com>
2024-04-25 16:43:02 -0300

    Add files via upload

---
+++ Avaliacao2/src/main/java/com/projetojpa/repository/AlunoRepository.java
@@ -0,0 +1,21 @@
+package com.projetojpa.repository;
+
+import java.util.List;
+
+import org.springframework.data.jdbc.repository.query.Query;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.query.Param;
+
+import com.projetojpa.entities.Aluno;
+
+public interface AlunoRepository extends JpaRepository<Aluno, Long>{
+	List<Aluno> findByRenda(Double renda);
+	List<Aluno> findByRa(String ra);
+	List<Aluno> findByCidadeAndRenda(String cidade, Double renda);
+	@Query("SELECT a FROM Aluno a WHERE a.nome = nome")
+	List<Aluno> findByNome(@Param("nome") String nome);
+	@Query("SELECT a FROM Aluno a WHERE a.nomeCompleto LIKE = nomeCompleto")
+	List<Aluno> findByNomeLike(@Param("nomeCompleto") String nomeCompleto);
+	@Query("SELECT a FROM Aluno a JOIN a.turma t WHERE t.id = turmaId")
+	List<Aluno> findByTurmaId(@Param("turmaId") Long turmaId);
+}
